2f8f9be994e76bf1c0804ae3c3b5569cd5633972
kBashar <khyrulbashar.0.1@gmail.com>
2023-04-06 14:51:58 +0600

    added prefix column to substation table

--- app/db/tables.py
+++ app/db/tables.py
@@ -9,7 +9,7 @@ from sqlalchemy import ForeignKey, PrimaryKeyConstraint
 from sqlalchemy import Integer, Boolean, Float
 from sqlalchemy import String
 from sqlalchemy import DateTime
-from sqlalchemy import text
+from sqlalchemy import inspect, text
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
@@ -21,6 +21,7 @@ class SubStation(Base):
     __tablename__ = TABLE_NAME_SUBSTATION
     name = Column(String, nullable=True, info={"verbose_name":"Name"})
     path = Column(String, primary_key=True, info={"verbose_name":"Server Path"})
+    # prefix = Column("prefix", String(100), nullable=True)
 
 class Tags(Base):
     __tablename__ = TABLE_NAME_TAG
@@ -47,6 +48,29 @@ class KeyStore(Base):
 
 def create_tables(engine):
     Base.metadata.create_all(engine)
+    update_tables(engine)
+
+def check_if_column_exists(engine, table, column_name):
+    inspector = inspect(engine)
+    columns = inspector.get_columns(table)
+    column_names = [c['name'] for c in columns]
+    return column_name in column_names
+
+def add_column(engine, table_name, column):
+    column_name = column.compile(dialect=engine.dialect)
+    column_type = column.type.compile(engine.dialect)
+    is_nullable = 'NULL' if column.nullable else 'NOT NULL'
+    with engine.connect() as conn:
+        sql_statement = f"ALTER TABLE {table_name} ADD COLUMN IF NOT EXISTS {column_name} {column_type} {is_nullable};"
+        conn.execute(text(sql_statement))
+
+  
+def update_tables(engine):
+    # add new columns to existing tables
+    # add prefix column to substation table
+    if not check_if_column_exists(engine, TABLE_NAME_SUBSTATION, "prefix"):
+       column = Column("prefix", String(100), nullable=True)
+       add_column(engine, TABLE_NAME_SUBSTATION, column)
 
 
 if __name__ == "__main__":
